bfa5cdc2ff4218bc78b951b940d511313e7ad5cb
kylin <kylin@mail>
2020-06-07 13:59:39 +0000

    make color :D

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -1,11 +1,9 @@
 @extends('basic-detail')
 
 @section('detail')
-    <div class="container">
     <video controls crossorigin playsinline poster="{{asset('img/'.$phim->background.'')}}">
             <source src="{{$publicUrl}}" type="video/mp4" size="576">
     </video>
-    </div>
 
     <script src="{{asset('js/plyr.js')}}"></script>
     <script>
