4605af81cfb45b335a221a3790d2e0e11b4f5bb0
kylin <kylin@mail>
2020-07-29 00:49:38 +0000

    just backup

--- resources/views/live.blade.php
+++ resources/views/live.blade.php
@@ -4,9 +4,19 @@
 
 @section('content')
 
-    <h1>9:53 pm 16/05(onedrive -> F12)</h1>
+    <h1>08:39 pm 29/07(IDM properties)</h1>
     <video width='400' height='200' controls='controls'>
-        <source src="https://f4-zvc.zdn.vn/81c4dec509e0e0beb9f1/8804250568024450928" type='video/mp4'/>
+        <source src="https://r1---sn-npoe7nez.googlevideo.com/videoplayback?expire=1595921639&ei=hoAfX--OOpODwgOnibqoBQ&ip=34.103.15.211&id=o-ACSlocPlZoxK0spR7OuOgeWmLCR8DwSmrRquhIG2fSoU&itag=137&aitags=133%2C134%2C135%2C136%2C137%2C160%2C242%2C243%2C244%2C247%2C248%2C278&source=youtube&requiressl=yes&mh=Zc&mm=31%2C26&mn=sn-npoe7nez%2Csn-hju7enel&ms=au%2Conr&mv=m&mvi=1&pl=23&gcr=vn&initcwndbps=1385000&vprv=1&mime=video%2Fmp4&gir=yes&clen=820087414&dur=5092.954&lmt=1595872254892237&mt=1595900009&fvip=1&keepalive=yes&fexp=23883098&beids=23886208&c=WEB&txp=5535432&sparams=expire%2Cei%2Cip%2Cid%2Caitags%2Csource%2Crequiressl%2Cgcr%2Cvprv%2Cmime%2Cgir%2Cclen%2Cdur%2Clmt&sig=AOq0QJ8wRAIgTf22gce3ryxs0awemn37_aWGRYmNEj5qpXTW5bLR8GwCICf9_nnrxl7wxVxtd0MzsZ7lbmt_zDTwlmkuUep9hvyX&lsparams=mh%2Cmm%2Cmn%2Cms%2Cmv%2Cmvi%2Cpl%2Cinitcwndbps&lsig=AG3C_xAwRAIgWKMXwoZhA-AueXRAmiCmi4MPjyd0YI4kNe0ErVxqAhcCIHvDidCJKBVUjUS2yJHxFtL0Qk8OHVvNSAg9J2kQZLbB" type='video/mp4'/>
     </video>
+
+    <h1>10:13 pm 29/07(IDM properties + 720p)</h1>
+    <video width='400' height='200' controls='controls'>
+        <source src="https://r3---sn-8qj-nbosd.googlevideo.com/videoplayback?expire=1595927523&ei=g5cfX4XkG5i8gAOx9pDwBA&ip=14.191.175.220&id=o-AGWTs6wUwMsXOCwf_3O5gkQAPDQ92XK_UUa2WylqwADj&itag=22&source=youtube&requiressl=yes&mh=V8&mm=31%2C29&mn=sn-8qj-nbosd%2Csn-8qj-nbole&ms=au%2Crdu&mv=m&mvi=3&pl=22&gcr=vn&initcwndbps=1361250&vprv=1&mime=video%2Fmp4&ratebypass=yes&dur=6990.181&lmt=1595681857758948&mt=1595905763&fvip=5&fexp=23883098&beids=23886208&c=WEB&txp=5532432&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cgcr%2Cvprv%2Cmime%2Cratebypass%2Cdur%2Clmt&sig=AOq0QJ8wRQIgDCfsle1e3oTFXMD5syU9FSwpHmrlIZc2yeynj2kCdv0CIQDsO-_CGDKJrMcGROWJBIzlGazl9ci2Q6gyFhwfLkhCnA%3D%3D&lsparams=mh%2Cmm%2Cmn%2Cms%2Cmv%2Cmvi%2Cpl%2Cinitcwndbps&lsig=AG3C_xAwRgIhAMiQu33faYrK9rl3kV8jm8VaLbkLzDSOjvrEzh6L33mMAiEAhUQpNqjpcv18l0N83unb7UOKLDN0BvdKtyld33bXaEI%3D" type='video/mp4'/>
+    </video>
+
+    <!-- <h1>10:13 pm 29/07(IDM properties + 1080p + mkv)</h1>
+    <video width='400' height='200' controls='controls'>
+        <source src="https://r3---sn-8qj-nbosd.googlevideo.com/videoplayback?expire=1595927523&ei=g5cfX4XkG5i8gAOx9pDwBA&ip=14.191.175.220&id=o-AGWTs6wUwMsXOCwf_3O5gkQAPDQ92XK_UUa2WylqwADj&itag=271&aitags=133%2C134%2C135%2C136%2C137%2C160%2C242%2C243%2C244%2C247%2C248%2C271%2C278&source=youtube&requiressl=yes&mh=V8&mm=31%2C29&mn=sn-8qj-nbosd%2Csn-8qj-nbole&ms=au%2Crdu&mv=m&mvi=3&pl=22&gcr=vn&initcwndbps=1361250&vprv=1&mime=video%2Fwebm&gir=yes&clen=2690465639&dur=6990.120&lmt=1595682067472299&mt=1595905763&fvip=5&keepalive=yes&fexp=23883098&beids=23886208&c=WEB&txp=5532432&sparams=expire%2Cei%2Cip%2Cid%2Caitags%2Csource%2Crequiressl%2Cgcr%2Cvprv%2Cmime%2Cgir%2Cclen%2Cdur%2Clmt&sig=AOq0QJ8wRQIgAWIak66A7--0wk1Gvt-9HpSuT5Csz9ISBC4CIEJwn8ACIQCruJUhmpmqhM6iZnHla3iF3j7t2qLw1jOprhHhRDZ3SA%3D%3D&lsparams=mh%2Cmm%2Cmn%2Cms%2Cmv%2Cmvi%2Cpl%2Cinitcwndbps&lsig=AG3C_xAwRgIhAMiQu33faYrK9rl3kV8jm8VaLbkLzDSOjvrEzh6L33mMAiEAhUQpNqjpcv18l0N83unb7UOKLDN0BvdKtyld33bXaEI%3D" type='video/mp4'/>
+    </video> -->
 @endsection
 
