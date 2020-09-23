0ee2a1045bb3d4f6d952b81bba0f38d1eb9bc03f
kylin <kylin@mail>
2020-09-23 23:58:23 +0000

    remove special check error ThienQuan

--- app/Http/Controllers/HiPhimController.php
+++ app/Http/Controllers/HiPhimController.php
@@ -183,10 +183,8 @@ class HiPhimController extends Controller
         //check living
         $isErrorUrl = false;
         if(!$this->urlExists($publicUrl)){
-            if($id!==120){
-                $isErrorUrl = true;
-                $this->baoloi($id,null);
-            }
+            $isErrorUrl = true;
+            $this->baoloi($id,null);
         }
         if($this->allowCount($id)){
             $phim->luotxem = $phim->luotxem+1;
